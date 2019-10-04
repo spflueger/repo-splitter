20abd3cd728fdda9c005e160fd3b0c28f563b617
Stefan Pflueger <spfluege@gmail.com>
2019-10-04 17:57:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+### How to use
+
+This script helps moving specific files from one repo into new one (including full history)
+It creates a small shell script that can be used when running the `git filter-branch` command.
+
+1. Run `python generateMoveScript.py --newroot path/you/want/to/strip/away /path/to/your/repo/root paths/to/move`
+  This creates a shell script `movescript.sh` that is used in the second step.
+
+2. `chmod u+x /path/to/script`
+
+3. Change directory into your repo that you want to move from, then run 
+  `git filter-branch -f --prune-empty --tree-filter /path/to/script HEAD`
+  You can verify the new log via `git log`
+
+4. Now actually make the changes by running `git filter-branch --prune-empty -f --subdirectory-filter newroot`
+
+5. If everything is good push to your new repo
---
+++ generateMoveScript.py
@@ -0,0 +1,54 @@
+import argparse
+from pathlib import Path
+import os
+import subprocess
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('repopath', metavar='repopath', type=str, nargs=1,
+                    help='path to the git repo')
+parser.add_argument('paths', metavar='paths', type=str, nargs='+',
+                    help='paths within the git repo to filter out from a repo into a new repo')
+parser.add_argument('--newroot', metavar='newroot', type=str,
+                    help='strip away this path from the newroot')
+
+args = parser.parse_args()
+
+wd = os.getcwd()
+os.chdir(args.repopath[0])
+files_to_move={}
+for path in args.paths:
+    for filename in Path(path).glob('**/*.*'):
+        files_to_move[filename] = []
+
+for key in files_to_move.keys():
+   vals = subprocess.check_output(["git log --name-only --format=format: --follow -- " + str(key) + " | sort -u"], shell=True)
+   vals = [x.decode("utf-8") for x in vals.split()]
+   files_to_move[key] = vals
+os.chdir(wd)
+
+if args.newroot:
+    old_files = files_to_move
+    files_to_move = {}
+    for key, vals in old_files.items():
+        if str(key).find(args.newroot) != 0:
+            raise RuntimeError("given paths within repo are not compatible for a newroot")
+        newkey = str(key)[len(args.newroot):]
+        newkey = "newroot/" + newkey.strip("/")
+        files_to_move[newkey] = vals
+
+temp_dirs_to_create = [os.path.split(x)[0] for x in files_to_move]
+dirs_to_create = set()
+for tempdir in temp_dirs_to_create:
+    if not [0 for x in temp_dirs_to_create if x != tempdir and x.find(tempdir) == 0]:
+        dirs_to_create.add(tempdir)
+
+f = open('movescript.sh', 'w')
+f.write("#!/bin/bash\n\n")
+for x in dirs_to_create:
+    f.write("mkdir -p "+str(x)+"\n")
+f.write("\n")
+for key, vals in files_to_move.items():
+    for val in vals:
+        f.write("mv " + str(val) + " " + str(key)+" 2>/dev/null\n")
+f.write("\ntrue")
+f.close()
